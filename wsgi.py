7640cf5f9f24c81b53c1a77334b977d114057d1f
huokr <docdedi@gmail.com>
2015-10-22 06:38:22 +0800

    -

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,32 @@
 from tornado.wsgi import WSGIApplication
 import os
+import sys
 from handlers.index import MainHandler
 from handlers.search import SearchHandler
 
 
 if __name__ == '__main__':
 
-    settings = {
-        'static_path': os.path.join(os.path.dirname(__file__), 'static'),
-        'name': __name__,
-    }
+    if 'OPENSHIFT_REPO_DIR' in os.environ:
+        sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi',))
+        virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/venv'
+        os.environ['PYTHON_EGG_CACHE'] = os.path.join(virtenv, 'lib/python3.3/site-packages')
+        virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+        try:
+            exec(compile(open(virtualenv).read(), virtualenv, 'exec'), dict(__file__ = virtualenv))
+        except IOError:
+            pass
+
+    if 'OPENSHIFT_REPO_DIR' in os.environ:
+        settings = {
+            'static_path': os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'static'),
+            'name': 'bot',
+        }
+    else:
+        settings = {
+            'static_path': os.path.join(os.getcwd(), 'static'),
+            'name': 'bot',
+        }
     app = WSGIApplication([
         (r'/', MainHandler),
         (r'/api/search', SearchHandler)
