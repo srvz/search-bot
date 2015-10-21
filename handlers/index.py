08d506b8ae6ee1402c5b9e8803f91d25472230f4
huokr <docdedi@gmail.com>
2015-10-22 07:31:51 +0800

    -

--- handlers/index.py
+++ handlers/index.py
@@ -6,7 +6,7 @@ env = Environment(loader=PackageLoader('handlers', '../templates'))
 js_path = '/static/js/main.js'
 if os.environ.get('PRODUCTION'):
     js_path = '/static/dist/main.min.js'
-    # js_path = 'http://7xnocn.com1.z0.glb.clouddn.com/main.min.js'
+    js_path = 'http://7xnocn.com1.z0.glb.clouddn.com/main.min.js'
 index_tpl = env.get_template('index.html').render(js_path=js_path)
 
 
