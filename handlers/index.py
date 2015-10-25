a9b686fefe1389064f6d7be88a8098046e8d4c11
huokr <docdedi@gmail.com>
2015-10-26 03:04:42 +0800

    -

--- handlers/index.py
+++ handlers/index.py
@@ -3,9 +3,11 @@ from jinja2 import Environment, PackageLoader
 import os
 
 env = Environment(loader=PackageLoader('handlers', '../templates'))
+
 js_path = '/static/js/main.js'
 if os.environ.get('PRODUCTION'):
     js_path = '/static/dist/main.min.js'
+else:
     js_path = 'http://7xnocn.com1.z0.glb.clouddn.com/main.min.js'
 index_tpl = env.get_template('index.html').render(js_path=js_path)
 
