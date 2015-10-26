b1cea6579bd0b0b2752e43281b8ad217da9e9cde
huokr <docdedi@gmail.com>
2015-10-26 19:02:39 +0800

    -

--- weixin/wxcrypt.py
+++ weixin/wxcrypt.py
@@ -56,7 +56,7 @@ class SHA1:
             sha = hashlib.sha1()
             sha.update("".join(sortlist))
             return ierror.WXBizMsgCrypt_OK, sha.hexdigest()
-        except Exception, e:
+        except Exception as e:
             # print e
             return ierror.WXBizMsgCrypt_ComputeSignature_Error, None
 
@@ -82,7 +82,7 @@ class XMLParse:
             encrypt = xml_tree.find("Encrypt")
             touser_name = xml_tree.find("ToUserName")
             return ierror.WXBizMsgCrypt_OK, encrypt.text, touser_name.text
-        except Exception, e:
+        except Exception as e:
             # print e
             return ierror.WXBizMsgCrypt_ParseXml_Error, None, None
 
@@ -159,7 +159,7 @@ class Prpcrypt(object):
             ciphertext = cryptor.encrypt(text)
             # 使用BASE64对加密后的字符串进行编码
             return ierror.WXBizMsgCrypt_OK, base64.b64encode(ciphertext)
-        except Exception, e:
+        except Exception as e:
             # print e
             return ierror.WXBizMsgCrypt_EncryptAES_Error, None
 
@@ -172,7 +172,7 @@ class Prpcrypt(object):
             cryptor = AES.new(self.key, self.mode, self.key[:16])
             # 使用BASE64对密文进行解码，然后AES-CBC解密
             plain_text = cryptor.decrypt(base64.b64decode(text))
-        except Exception, e:
+        except Exception as e:
             # print e
             return ierror.WXBizMsgCrypt_DecryptAES_Error, None
         try:
@@ -185,7 +185,7 @@ class Prpcrypt(object):
             xml_len = socket.ntohl(struct.unpack("I", content[: 4])[0])
             xml_content = content[4: xml_len + 4]
             from_appid = content[xml_len + 4:]
-        except Exception, e:
+        except Exception as e:
             # print e
             return ierror.WXBizMsgCrypt_IllegalBuffer, None
         if from_appid != appid:
